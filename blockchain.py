6dee185113e94e5d8184bb219073d8d15a0af968
Tanishq Jasoria <jasoriatanishq@gmail.com>
2020-01-01 16:07:46 +0530

    Define the new_transaction function

--- blockchain.py
+++ blockchain.py
@@ -25,19 +25,41 @@ class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
-    
+
+
+
     def new_block(self):
         # Creates a new Block and adds it to the chain
         pass
-    
-    def new_transaction(self):
-        # Adds a new transaction to the list of transactions
-        pass
-    
+
+
+
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Creates a new transaction to go into the next mined Block
+        :param sender: Address of the Sender
+        :param recipient: Address of the Recipient
+        :param amount: Amount
+        :return: The index of the Block that will hold this transaction
+        """
+        self.current_transactions.append(
+            {
+                'sender' : sender,
+                'recipient' : recipient,
+                'amount' : amount,
+            }
+        )
+
+        return self.last_block['index'] + 1
+
+
+
     @staticmethod
     def hash(block):
         # Hashes a Block
         pass
+
+
 
     @property
     def last_block(self):
