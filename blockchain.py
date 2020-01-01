acd81c18b997d2656f8051b3c177ae7566b6f298
Tanishq Jasoria <jasoriatanishq@gmail.com>
2020-01-01 16:34:57 +0530

    Add the calulation mechanism of proof of work in blockchain

--- blockchain.py
+++ blockchain.py
@@ -7,7 +7,7 @@ from time import time
 
 class Blockchain(object):
   """
-  This class is reponsible form managing the chain. It will store
+  This class is responsible form managing the chain. It will store
   transactions and have some helper methods for adding new blocks
   to the chain.
 
@@ -35,6 +35,7 @@ class Blockchain(object):
 
     # Create the genesis block
     self.new_block(previous_hash=1, proof=100)
+
 
 
   def new_block(self, proof, previous_hash=None):
@@ -98,3 +99,36 @@ class Blockchain(object):
   def last_block(self):
 
     return self.chain[-1]
+
+
+
+  def proof_of_work(self, last_proof):
+    """
+    Simple Proof of Work Algorithm:
+     - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+     - p is the previous proof, and p' is the new proof
+    :param last_proof: value of last Proof
+    :return: new value of Proof of work
+    """
+
+    proof = 0
+    while self.valid_proof(last_proof, proof) is False:
+      proof += 1
+
+    return proof
+
+
+
+  @staticmethod
+  def valid_proof(last_proof, proof):
+    """
+    Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
+    :param last_proof: value of last Proof
+    :param proof: current possible value of Proof
+    :return: True if correct, False if not.
+    """
+
+    guess = f'{last_proof}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:4] == "0000"
+  
