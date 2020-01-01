0463852ace2b34131b4dcbb2487a1b86071c3d90
Tanishq Jasoria <jasoriatanishq@gmail.com>
2020-01-01 16:43:20 +0530

    Blueprint of our blockchain API

---
+++ blockchain_api.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from uuid import uuid4
+
+# Instantiate our Node
+
+# Generate a globally unique address for this node
+
+# Instantiate the Blockchain
+
+"""
+We will use Flask framework to talk to our Blockchain using HTTP requests
+
+We will be creating three methods:
+  - /transactions.new to create a new transaction to a block
+  - /mine to tell our server to mine a new block
+  - /chain to return the full Blockchain
+
+Our server will from a single nonde in our blockchain network
+"""
+
