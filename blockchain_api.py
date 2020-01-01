14978f648704afedbfa4e7ee6be38f537283281d
Tanishq Jasoria <jasoriatanishq@gmail.com>
2020-01-01 16:52:53 +0530

    Write the new_transaction API endpoint, get data through POST

--- blockchain_api.py
+++ blockchain_api.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from uuid import uuid4
+from blockchain import Blockchain
 
 # Instantiate our Node
+app = Flask(__name__)
 
 # Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('-', '')
 
 # Instantiate the Blockchain
+blockchain = Blockchain()
 
 """
 We will use Flask framework to talk to our Blockchain using HTTP requests
@@ -18,3 +22,34 @@ We will be creating three methods:
 Our server will from a single nonde in our blockchain network
 """
 
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "We'll mine a new Block"
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+
+    values = request.get_json()
+
+    # Check that the required fields are in the POST'ed data
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    # Create a new Transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'],
+                                       values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
